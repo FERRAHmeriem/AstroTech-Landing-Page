49b0dd271fe86b1e8448dbc72fbe909041ca5232
Meriem FERRAH <149694634+FERRAHmeriem@users.noreply.github.com>
2025-10-21 01:56:12 +0100

    Update aboutUsHeroSection.js

--- src/components/aboutUsHeroSection.js
+++ src/components/aboutUsHeroSection.js
@@ -39,7 +39,7 @@ export default function AboutUsHeroSection() {
         </div>
       </div>
 
-      <div className="absolute bottom-[-150px] md:bottom-[-250px] lg:bottom-[-350px] left-1/2 -translate-x-1/2 w-full">
+      <div className="absolute bottom-[-250px] md:bottom-[-350px] lg:bottom-[-450px] left-1/2 -translate-x-1/2 w-full">
         <div className="relative flex flex-col md:flex-row justify-center items-center gap-12 md:gap-20 lg:gap-32 px-4">
           
           {/* Counter */}
